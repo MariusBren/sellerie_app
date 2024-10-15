6f23e4e1abd3e5503ac18623fe7c9380a4b7361f
MariusBren <mariusbrennetuit@yahoo.fr>
2024-10-15 09:41:28 +0200

    Ajout crud category + update crud product

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,30 +3,23 @@
 namespace App\Form;
 
 use App\Entity\Category;
-use App\Entity\Product;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('product_name')
-            ->add('description')
-            ->add('category', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'categoryName',
-            ])
+            ->add('category_name')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Product::class,
+            'data_class' => Category::class,
         ]);
     }
 }
