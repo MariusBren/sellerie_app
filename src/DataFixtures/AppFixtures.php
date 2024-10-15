952e397ada5c50e34fc10bec7732ec7d0fe0a6b5
MariusBren <mariusbrennetuit@yahoo.fr>
2024-10-15 09:01:12 +0200

    Ajout entité category + MAJ fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,8 +3,9 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use App\Entity\Product;
 use DateTimeImmutable;
+use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -17,12 +18,27 @@ class AppFixtures extends Fixture
 
         $faker = Factory::create('fr_FR');
 
+        $categories = [];
+
+        // Création des catégories - début
+        for ($p=0; $p < mt_rand(5, 10); $p++) {
+
+            $category = new Category();
+            $category->setCategoryName($faker->word());
+            $manager->persist($category);
+
+            $categories[] = $category;
+
+        }
+        // Création des catégories - fin
+
         // Création des produits - début
         for ($p=0; $p < mt_rand(50, 100); $p++) {
 
             $product = new Product();
             $product->setProductName($faker->word());
             $product->setDescription($faker->sentence());
+            $product->setCategory($faker->randomElement($categories));
             $manager->persist($product);
         }
         // Création des produits - fin
