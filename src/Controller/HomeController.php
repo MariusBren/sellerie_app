7cd5d9186ace54d6d04456eac766bf4a45d9a6c3
MariusBren <mariusbrennetuit@yahoo.fr>
2024-12-10 17:24:52 +0100

    Ajout HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        // Redirige vers l'accueil (index produit)
+        return $this->redirectToRoute('app_product_index');
+    }
+}
